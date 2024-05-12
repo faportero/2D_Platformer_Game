27e92cfce9b5687c550a6157f46ce4b6bd4f334f
faportero <fabianportero94@gmail.com>
2024-05-11 20:09:53 -0500

    Curso  - 17. Cinemachine Camara

---
+++ Assets/Assets/Effects/RippleEffect/RippleEffect/RippleEffect.shader
@@ -0,0 +1,88 @@
+﻿Shader "Custom/Ripple Effect"
+{
+    Properties
+    {
+        _MainTex("Base", 2D) = "white" {}
+        _GradTex("Gradient", 2D) = "white" {}
+        _Reflection("Reflection Color", Color) = (0, 0, 0, 0)
+        _Params1("Parameters 1", Vector) = (1, 1, 0.8, 0)
+        _Params2("Parameters 2", Vector) = (1, 1, 1, 0)
+        _Drop1("Drop 1", Vector) = (0.49, 0.5, 0, 0)
+        _Drop2("Drop 2", Vector) = (0.50, 0.5, 0, 0)
+        _Drop3("Drop 3", Vector) = (0.51, 0.5, 0, 0)
+    }
+
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            sampler2D _MainTex;
+            sampler2D _GradTex;
+            float4 _Reflection;
+            float4 _Params1;    // [ aspect, 1, scale, 0 ]
+            float4 _Params2;    // [ 1, 1/aspect, refraction, reflection ]
+            float3 _Drop1;
+            float3 _Drop2;
+            float3 _Drop3;
+
+            float wave(float2 position, float2 origin, float time)
+            {
+                float d = length(position - origin);
+                float t = time - d * _Params1.z;
+                return (tex2D(_GradTex, float2(t, 0)).a - 0.5f) * 2;
+            }
+
+            float allwave(float2 position)
+            {
+                return
+                    wave(position, _Drop1.xy, _Drop1.z) +
+                    wave(position, _Drop2.xy, _Drop2.z) +
+                    wave(position, _Drop3.xy, _Drop3.z);
+            }
+
+            v2f vert(appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                return o;
+            }
+
+            fixed4 frag(v2f i) : SV_Target
+            {
+                const float2 dx = float2(0.01f, 0);
+                const float2 dy = float2(0, 0.01f);
+                float2 p = i.uv * _Params1.xy;
+
+                float w = allwave(p);
+                float2 dw = float2(allwave(p + dx) - w, allwave(p + dy) - w);
+
+                float2 duv = dw * _Params2.xy * 0.2f * _Params2.z;
+                half4 c = tex2D(_MainTex, i.uv + duv);
+                float fr = pow(length(dw) * 3 * _Params2.w, 3);
+
+                return lerp(c, _Reflection, fr);
+            }
+            ENDCG
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,11 +10,13 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
     private Vector2 direction;
+    private CinemachineVirtualCamera cm;
 
     [Header("Statistics")]
     public float velocity = 10;
     public float jumpStrength = 5;
-    
+    public float dashVelocity = 20;
+
     [Header("Collisions")]
     public Vector2 down;    
     public float collisionRatio;
@@ -23,10 +26,17 @@ public class PlayerMovement : MonoBehaviour
     public bool canMove;
     public bool isGrounded;
     private bool canDoubleJump;
+    public bool canDash;
+    public bool doingDash;
+    public bool tapFloor;
+    public bool doingShake = false;
+    
     private void Awake()
+
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        cm = GameObject.FindGameObjectWithTag("VirtualCamera").GetComponent<CinemachineVirtualCamera>();
     }
     void Start()
     {
@@ -40,10 +50,82 @@ public class PlayerMovement : MonoBehaviour
         CheckGround();
     }
 
+    private IEnumerator CameraShake()
+    {
+        doingShake = true;
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = cm.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 5;
+
+        yield return new WaitForSeconds(0.3f);
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0;
+        doingShake = false;
+    }
+
+    private IEnumerator CameraShake(float tiempo)
+    {
+        doingShake = true;
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin = cm.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 5;
+
+        yield return new WaitForSeconds(tiempo);
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0;
+        doingShake = false;
+    }
+
+
+    private void Dash(float x, float y) 
+    {
+        anim.SetBool("Dash",true);
+        Vector3 playerPosition = Camera.main.WorldToViewportPoint(transform.position);
+        Camera.main.GetComponent<RippleEffect>().Emit(playerPosition);
+        StartCoroutine(CameraShake());
+
+        canDash = true; 
+        rb.velocity = Vector2.zero;
+        rb.velocity += new Vector2(x, y).normalized * dashVelocity;
+        StartCoroutine(PreDash());
+    }
+
+    private IEnumerator PreDash()
+    {
+        StartCoroutine(FloorDash());
+        rb.gravityScale = 0;
+        doingDash = true;  
+        
+        yield return new WaitForSeconds(0.3f);
+        rb.gravityScale = 3;
+        doingDash = false;
+        EndDash();
+    }
+
+    private IEnumerator FloorDash()
+    {
+        yield return new WaitForSeconds(0.15f);
+        if(isGrounded)
+        {
+            canDash = false;
+        }
+    }
+
+    public void EndDash()
+    {
+        anim.SetBool("Dash", false);
+    }
+
+    private void TapFloor()
+    {
+        canDash=false;
+        doingDash=false;
+        anim.SetBool("Jump", false);
+    }
+
     private void Movement()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
+
+        float xRaw = Input.GetAxisRaw("Horizontal");
+        float yRaw = Input.GetAxisRaw("Vertical");
 
         direction = new Vector2(x, y);
 
@@ -70,6 +152,26 @@ public class PlayerMovement : MonoBehaviour
                     }
                 }
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.X) && !doingDash)
+        {
+            if(xRaw !=0 || yRaw !=0)
+            {
+                Dash(xRaw, yRaw);
+            }
+        }
+
+
+        if(isGrounded && !tapFloor)
+        {
+            TapFloor();
+            tapFloor = true;
+        }
+
+        if(!isGrounded && tapFloor)
+        {
+            tapFloor = false;
         }
 
         float velocity;
@@ -116,7 +218,7 @@ public class PlayerMovement : MonoBehaviour
     }
     private void Walk()
     {   
-        if(canMove) 
+        if(canMove && !doingDash) 
         {
             rb.velocity = new Vector2(direction.x * velocity, rb.velocity.y);
             
